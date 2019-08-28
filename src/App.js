5538be9aea80184e2dd237090db38f4a71decc3b
CoderX. Zeika <zekapako29@gmail.com>
2019-08-28 18:37:23 +0700

    up

--- src/App.js
+++ src/App.js
@@ -1,66 +1,74 @@
-import React, { Component } from 'react';
-import Header from './Components/Header';
-import SideBar from './Components/SideBar';
-import Slide from './Components/Slide';
-import Products from './Components/Products';
-import Footer from './Components/Footer';
-import Cart from './Components/Cart'
-import './App.css'
+import React, { Component } from "react";
+import Header from "./Components/Header";
+import SideBar from "./Components/SideBar";
+import Slide from "./Components/Slide";
+import Products from "./Components/Products";
+import Footer from "./Components/Footer";
+import Cart from "./Components/Cart";
+import "./App.css";
 
 class App extends Component {
   constructor(props) {
-    super(props)
+    super(props);
     this.state = {
       listDT: [
         {
           id: 1,
-          name: 'Iphone 6 plus',
+          name: "Iphone 6 plus",
           price: 700,
-          desc: 'Điện thoại của Apple',
-          image:'https://cdn.tgdd.vn/Products/Images/42/87846/iphone-6s-plus-32gb-400x460.png',
-          star : 4
+          desc: "Điện thoại của Apple",
+          image:
+            "https://cdn.tgdd.vn/Products/Images/42/87846/iphone-6s-plus-32gb-400x460.png",
+          star: 4
         },
         {
           id: 2,
-          name: 'Nokia 1202',
+          name: "Nokia 1202",
           price: 900,
-          desc: 'Điện thoại nokia ',
-          image: 'https://didongso.com.vn/wp-content/uploads/2018/09/nokia12-2.png',
+          desc: "Điện thoại nokia ",
+          image:
+            "https://didongso.com.vn/wp-content/uploads/2018/09/nokia12-2.png",
           star: 5
         },
         {
           id: 3,
-          name: 'Iphone Xs max',
+          name: "Iphone Xs max",
           price: 1700,
-          desc: 'Điện thoại mới nhất của Apple',
-          image:'https://cdn.tgdd.vn/Products/Images/42/190321/iphone-xs-max-gold-400x460.png',
-          star : 5
+          desc: "Điện thoại mới nhất của Apple",
+          image:
+            "https://cdn.tgdd.vn/Products/Images/42/190321/iphone-xs-max-gold-400x460.png",
+          star: 5
         },
         {
           id: 4,
-          name: 'Fmobile T12',
+          name: "Fmobile T12",
           price: 700,
-          desc: 'Điện thoại của FMobile',
-          image:'https://img.websosanh.vn/v2/users/dclimg/images/ufdrie60ez77o.jpg',
-          star:3
+          desc: "Điện thoại của FMobile",
+          image:
+            "https://img.websosanh.vn/v2/users/dclimg/images/ufdrie60ez77o.jpg",
+          star: 3
         },
         {
           id: 5,
-          name: 'Vinsmart Joy1',
+          name: "Vinsmart Joy1",
           price: 700,
-          desc: 'Điện thoại của Vin',
-          image: 'https://cdn.fptshop.com.vn/Uploads/Originals/2018/12/11/636801240961123743_vsmart-joy1-plus-den-1.png',
-          star:4
+          desc: "Điện thoại của Vin",
+          image:
+            "https://cdn.fptshop.com.vn/Uploads/Originals/2018/12/11/636801240961123743_vsmart-joy1-plus-den-1.png",
+          star: 4
         }
       ]
-    
-    }
+    };
   }
-
+  proa = params => {
+    console.log(params);
+  };
+  addToCart = id => {
+    console.log(id);
+  };
   render() {
-
     return (
-      <div >
+      <div>
         <div>
           <Header />
           {/* Page Content */}
@@ -70,21 +78,23 @@ class App extends Component {
               {/* /.col-lg-3 */}
               <div className="col-lg-9">
                 <Slide />
-                <Products products = {this.state.listDT}/>
+                <Products
+                  products={this.state.listDT}
+                  send={this.proa}
+                  addToCart={this.addToCart}
+                />
                 {/* /.row */}
               </div>
               {/* /.col-lg-9 */}
             </div>
             {/* /.row */}
 
-            <Cart/>
-
+            <Cart />
           </div>
           {/* /.container */}
           {/* Footer */}
           <Footer />
         </div>
-
       </div>
     );
   }
--- src/Components/Products.js
+++ src/Components/Products.js
@@ -1,19 +1,26 @@
-import React, { Component } from 'react'
-import ProductItem from './ProductItem'
+import React, { Component } from "react";
+import ProductItem from "./ProductItem";
 export default class Products extends Component {
-    showProducts = (products) => {
-        if (products)
-            return products.map((item, index)=>{
-                return <ProductItem item = {item} key = {index} />
-            })
-    }
-
-    render() {
-        var products = this.props.products;
+  showProducts = products => {
+    if (products)
+      return products.map((item, index) => {
         return (
-            <div className="row">
-                {this.showProducts(products)}
-            </div>
-        )
-    }
+          <ProductItem
+            item={item}
+            key={index}
+            addToCart={this.props.addToCart}
+          />
+        );
+      });
+  };
+
+  render() {
+    var products = this.props.products;
+    return (
+      <div className="row">
+        {this.showProducts(products)}
+        {this.props.send("this is string")}
+      </div>
+    );
+  }
 }
